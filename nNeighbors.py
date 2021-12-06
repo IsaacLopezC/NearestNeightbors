fceded46e16564fe7621b8e2e8e8a123bc045f88
IsaacLopez <isaaclopez314@aragon.unam.mx>
2021-12-05 18:59:52 -0600

    Agregando modulos y descripcion de los mismos

--- nNeighbors.py
+++ nNeighbors.py
@@ -1,5 +1,4 @@
 
-from os import name
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
@@ -10,29 +9,60 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 
-
-def main():
-    #Encabezados para archivos csv
+def getCSV(featuresRoute, testRoute):
+    '''
+    Devuelve los archivos CSV en forma de Dateframe para poder ser utilizados
+    
+    Parametros
+    -----------
+    featuresRoute : str
+        la ruta al archivo features.csv
+        
+    testRoute : str
+        la ruta al archivo respuesta.csv
+        
+    Returns
+    -----------
+    featureData, testData : Dataframe
+        Los dos archivos CSV convertido a dataframe
+    '''
     dataHead = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
     testHead = ['C1']
-
-    # LEyendo archivos csv
-    featureData = pd.read_csv('./data/features.csv',
-                              names=dataHead, na_values=['no info', '?'])
-    testData = pd.read_csv('./data/respuesta.csv',
-                           names=testHead, na_values=['no info', '?'])
-    #Convirtiendo NaN a 0
+    featureData = pd.read_csv(featuresRoute, names=dataHead, na_values=['no info', '?'])
+    testData = pd.read_csv(testRoute, names=testHead, na_values=['no info', '?'])
     featureData = featureData.fillna(0)
     testData = testData.fillna(0)
     
+    return featureData,testData
+
+def nearestNeightborModel(featureData,testData):
+    '''
+    Recibe 2 dataframe para crear y entrenar un modelo de vecino mas cercano
+    
+    Parametros
+    -------------
+    featureData, testData : Dataframe
+        recibe los 2 dataframe
+        
+    Returns
+    ---------------
+    modelo : kNeighborsClassifier
+        el clasificador ya entrenado
+        
+    x_trainData, y_trainData : list
+        los datos de entrenamiento divididos
+        
+    x_testData, y_testData : list
+        los datos de prueba divididos
+    '''
     # Normalizando Datos 
     scaler = StandardScaler()
 
     scaler.fit(featureData)
     scaled_features = scaler.transform(featureData)
     scaled_data = pd.DataFrame(scaled_features, columns=featureData.columns)
-
-    #Dividiendo Muestra para entrenamiento y prueba donde X es scaled_data y Y es testData
+    
+      #Dividiendo Muestra para entrenamiento y prueba donde X es scaled_data y Y es testData
     x_trainData, x_testData, y_trainData, y_testData = train_test_split(
         scaled_data, testData['C1'], test_size=0.2)
 
@@ -40,12 +70,37 @@ def main():
     modelo = KNeighborsClassifier(n_neighbors=1)
     #Entrenando el modelo
     modelo.fit(x_trainData, y_trainData)
-    #Probando modelo
+    
+    return modelo,x_trainData, x_testData, y_trainData, y_testData
+
+def evalua(y_testData,prediccion):
+    '''
+    Imprime el desempeño de la tecnica.
+    
+    Parametros
+    ----------------
+    y_testData : list
+        Lista de los valores usados para las pruebas
+    prediccion : ndarray
+        prediccion del modelo para los datos provistos
+        
+    Returns
+    ---------------
+    Void
+    '''
+    print(classification_report(y_testData, prediccion))
+    print(confusion_matrix(y_testData, prediccion))
+
+def main():
+    
+    featureData,testData = getCSV('./data/features.csv','./data/respuesta.csv')
+    
+    modelo,x_trainData, x_testData, y_trainData, y_testData = nearestNeightborModel(featureData,testData)
+    
     prediccion = modelo.predict(x_testData)
 
     # Generando matriz de confusion y rep de clasificacion
-    print(classification_report(y_testData, prediccion))
-    print(confusion_matrix(y_testData, prediccion))
+    evalua(y_testData,prediccion)
 
     # Obteniendo mejor K
     rango = range(1, 40)
